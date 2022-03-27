672bbacdcc9e0b0576b8dee7ef93d25faf6dee78
Franco Canevali <francocanevali@gmail.com>
2022-03-27 15:07:37 -0300

    added polybar

---
+++ .config/polybar/launch_polybar.sh
@@ -0,0 +1,10 @@
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
+
+polybar primary &
+polybar secondary
